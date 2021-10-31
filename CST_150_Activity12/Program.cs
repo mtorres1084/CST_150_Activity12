90f20c7da3e39d8b61bb79a1456afa07c5514d53
Miguel Torres Perez <silverwolf1084@outlook.com>
2021-10-31 19:44:55 -0400

    Add project files.

---
+++ CST_150_Activity12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace CST_150_Activity12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "";
+            if (args.Length == 0)
+            {
+                text = File.ReadAllText(@"../../testFile.txt");
+            }else
+            {
+                text = File.ReadAllText(args[0]);
+            }
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            //string text = "This is a test provided by me. \n test " +
+            //    "this is another test";
+            Int64 matchCount = Regex.Matches(text, "([te])(?![a-z])", RegexOptions.Multiline | RegexOptions.IgnoreCase).Count;
+            Console.WriteLine("There are "+matchCount+" words that end in t or e");
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+    }
+}
